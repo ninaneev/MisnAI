ba68b58efeca351ad9c80663eee718e1afbf37fc
Nina Cressoni <ninacressoni@gmail.com>
2026-04-15 17:44:35 -0300

    Fix Vite file watching on Windows

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,16 @@
+/// <reference types="vitest" />
+
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  server: {
+    watch: {
+      usePolling: true,
+      interval: 100,
+    },
+  },
   test: {
     globals: true,
     environment: 'jsdom',
